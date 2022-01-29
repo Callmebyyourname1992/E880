a439d98f63478610103b0455c1b7cd0e02dae9d7
Callmebyyourname1992 <76661956+Callmebyyourname1992@users.noreply.github.com>
2022-01-29 10:02:40 -0600

    Assignment 1
    Add assignment 1

--- E880.m
+++ E880.m
@@ -1,3 +1,4 @@
-x = 9478657
-y = 2298912
-myfunction(x,y)
+x = 9478657;
+y = 2298912;
+result = myfunction(x,y);
+result1 = myfunction1(x,y);
---
+++ myfunction1.m
@@ -0,0 +1,3 @@
+function result = myfunction1(x,y)
+result = x^4*(-2 + 2*x^2 - x^4) +y^4*(578*x^2+83521*y^4);
+end
