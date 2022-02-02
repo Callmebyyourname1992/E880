18a60e236305288134aae0caf802d6ab4aa6344d
Callmebyyourname1992 <76661956+Callmebyyourname1992@users.noreply.github.com>
2022-02-02 08:03:50 -0600

    Add question 1a and latex

--- myfunction.m
+++ myfunction.m
@@ -1,5 +1,5 @@
 function result = myfunction(x,y)
-    result = 83521*y^8 +572*x^2*y^4 -2*x^4+2*x^6-x^8;
+    result = 83521*y^8 + 572*x^2*y^4-2*x^4+2*x^6-x^8
 end   
 
 
--- myfunction1.m
+++ myfunction1.m
@@ -1,3 +1,3 @@
 function result = myfunction1(x,y)
-result = x^4*(-2 + 2*x^2 - x^4) +y^4*(578*x^2+83521*y^4);
+result = 83521*y^8+x^2*(722y*4+x^2*(-2-x^2))
 end
